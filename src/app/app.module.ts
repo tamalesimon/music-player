66f63d2a4cd451b787c0f8f159670ad7fc72e651
simonpeter.tamale <simonpeter.tamale@equitybank.co.ug>
2022-10-10 09:46:25 +0300

    update the layout and look of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SongComponent } from './song/song.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
